c3a543ad0765fb300b1e5144ab6cb91481778419
anonymous anonymous <terry.goguen@kellogg.northwestern.edu>
2022-02-20 18:03:32 +0000

    Generate graphiti resources for neighborhoods

---
+++ app/resources/neighborhood_resource.rb
@@ -0,0 +1,10 @@
+class NeighborhoodResource < ApplicationResource
+  attribute :id, :integer, writable: false
+  attribute :created_at, :datetime, writable: false
+  attribute :updated_at, :datetime, writable: false
+
+  # Direct associations
+
+  # Indirect associations
+
+end
